64466b2cca15c3e4e348d4cae8e7ca1f98e9b69b
Sumiaya Akther <sumiayaakther15@gmail.com>
2025-07-08 21:02:18 +0600

    riders pending and active status api and day 5 done

--- index.js
+++ index.js
@@ -164,7 +164,50 @@ async function run() {
             const rider = req.body;
             const result = await ridersCollection.insertOne(rider);
             res.send(result);
-        })
+        });
+
+
+        app.get("/riders/pending", async (req, res) => {
+            try {
+                const pendingRiders = await ridersCollection
+                    .find({ status: "pending" })
+                    .toArray();
+
+                res.send(pendingRiders);
+            } catch (error) {
+                console.error("Failed to load pending riders:", error);
+                res.status(500).send({ message: "Failed to load pending riders" });
+            }
+        });
+
+
+        app.get("/riders/active", async (req, res) => {
+            const result = await ridersCollection.find({ status: "active" }).toArray();
+            res.send(result);
+        });
+
+
+        app.patch("/riders/:id/status", async (req, res) => {
+            const { id } = req.params;
+            const { status } = req.body;
+            const query = { _id: new ObjectId(id) }
+            const updateDoc = {
+                $set:
+                {
+                    status
+                }
+            }
+
+            try {
+                const result = await ridersCollection.updateOne(
+                    query, updateDoc
+
+                );
+                res.send(result);
+            } catch (err) {
+                res.status(500).send({ message: "Failed to update rider status" });
+            }
+        });
 
 
         //riders----------------->
